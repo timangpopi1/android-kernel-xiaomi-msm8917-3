bdb0dd2605ba71e728f63b95b916115f9e53e351
TheHitMan7 <krtik.vrma@gmail.com>
2019-08-18 14:30:07 +0530

    BACKPORT: sched/fair: Re-Introduce an energy estimation helper function
    The previous implementation can be found in below commit.
    https://github.com/TheHitMan7/kernel_xiaomi_msm8953/commit/dc9b764b5e5bbd0e68c7fbb6c3502b793270e765
    
    It has been modified for the following reason:
    
    BACKPORT: Reworked cpu_util_next() to work with
    WALT. Removed the call to schedutil_energy_util() which means we only
    look at CFS signals for the energy calculation. That last point
    shouldn't make a huge difference as RT and DL pressure should hopefully
    be small, and we don't have the PELT signals for these classes in 4.9.
    
    But now, we have both PELT and UTIL_EST implementation in Linux kernel 3.18.y
    So add back the original mainline implementation for estimated utilization.
    
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6832,22 +6832,7 @@ static void nohz_balancer_kick(bool only_update);
 static unsigned long cpu_util_next(int cpu, struct task_struct *p, int dst_cpu)
 {
 	struct cfs_rq *cfs_rq = &cpu_rq(cpu)->cfs;
-	unsigned long util;
-
-#ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
-		/*
-		 * Since WALT doesn't have blocked util, there is no need to
-		 * remove the task contribution during wake-up. Just add it to
-		 * the destination CPU.
-		 */
-		util = cpu_util(cpu);
-		if (cpu == dst_cpu)
-			util += task_util(p);
-
-		return util;
-	}
-#endif
+	unsigned long util_est, util = READ_ONCE(cfs_rq->avg.util_avg);
 
 	/*
 	 * If @p migrates from @cpu to another, remove its contribution. Or,
@@ -6855,11 +6840,25 @@ static unsigned long cpu_util_next(int cpu, struct task_struct *p, int dst_cpu)
 	 * the other cases, @cpu is not impacted by the migration, so the
 	 * util_avg should already be correct.
 	 */
-	util = READ_ONCE(cfs_rq->avg.util_avg);
 	if (task_cpu(p) == cpu && dst_cpu != cpu)
-		sub_positive(&util, p->se.avg.util_avg);
+		sub_positive(&util, task_util(p));
 	else if (task_cpu(p) != cpu && dst_cpu == cpu)
-		util += p->se.avg.util_avg;
+		util += task_util(p);
+
+	if (sched_feat(UTIL_EST)) {
+		util_est = READ_ONCE(cfs_rq->avg.util_est.enqueued);
+
+		/*
+		 * During wake-up, the task isn't enqueued yet and doesn't
+		 * appear in the cfs_rq->avg.util_est.enqueued of any rq,
+		 * so just add it (if needed) to "simulate" what will be
+		 * cpu_util() after the task has been enqueued.
+		 */
+		if (dst_cpu == cpu)
+			util_est += _task_util_est(p);
+
+		util = max(util, util_est);
+	}
 
 	return min(util, capacity_orig_of(cpu));
 }
